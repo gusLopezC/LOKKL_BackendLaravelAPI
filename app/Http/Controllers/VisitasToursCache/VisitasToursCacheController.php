c1f81ea675fb1193b338a82d19656e8a7cef478e
Gustavo <guslopezcallejas@gmail.com>
2020-02-20 00:29:54 -0600

    Prueba antes de morir

--- app/Http/Controllers/VisitasToursCache/VisitasToursCacheController.php
+++ app/Http/Controllers/VisitasToursCache/VisitasToursCacheController.php
@@ -101,7 +101,8 @@ class VisitasToursCacheController extends Controller
                     'tours.calification',
                     'tours.price',
                     'tours.moneda',
-                    'photos_tours.photo')
+                    'photos_tours.photo'
+                )
                 ->join('tours', 'visitas_tours_caches.id_tour', '=', 'tours.id')
                 ->join('users', 'visitas_tours_caches.user_id', '=', 'users.id')
                 ->join('photos_tours', 'tours.id', '=', 'photos_tours.tour_id')
@@ -132,7 +133,7 @@ class VisitasToursCacheController extends Controller
                         'tours.calification',
                         'tours.price',
                         'tours.moneda',
-                        'photos_tours.photo',
+                        'photos_tours.photo'
                     )
                     ->join('photos_tours', 'tours.id', '=', 'photos_tours.tour_id')
                     ->where('tours.pais', '=', $visitas[0]->pais)
